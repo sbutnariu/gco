d54ad843e70dfc13d26716acd14ce885052e6fb8
sbutnariu <sbutnariu@pentalog.fr>
2016-10-17 18:15:11 +0300

    ut(feat): test uts jenkins

--- src/GcoBundle/Controller/UserController.php
+++ src/GcoBundle/Controller/UserController.php
@@ -4,20 +4,26 @@ namespace GcoBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use GcoBundle\DataFixture\UserDataFixture;
 
 class UserController
 {
     private $dataFixture;
-    public function __construct(UserDataFixture $dataFixture)
+    private $serializer;
+
+    public function __construct(UserDataFixture $dataFixture, SerializerInterface $serializer)
     {
         $this->dataFixture = $dataFixture;
+        $this->serializer = $serializer;
     }
 
     public function getUserAction(Request $request, $id)
     {
         $user = $this->dataFixture->getUser($id);
-        var_dump($user);
-        return new Response($id, 200);
+        $jsonContent = $this->serializer->serialize($user, JsonEncoder::FORMAT);
+
+        return new Response($jsonContent, 200);
     }
 }
